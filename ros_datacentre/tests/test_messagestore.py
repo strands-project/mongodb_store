b1373f45ad140d78609853fefb6cd5b989f9657f
cburbridge <cburbridge@gmail.com>
2014-04-10 13:27:41 +0100

    Adding an initial rostest for the message store proxy.

---
+++ ros_datacentre/tests/test_messagestore.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+import unittest
+import random
+from ros_datacentre.message_store import MessageStoreProxy
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+class TestMessageStoreProxy(unittest.TestCase):
+
+    def test_add_message(self):
+        msg_store = MessageStoreProxy()
+        POSE_NAME = "test_pose_6"
+        p = Pose(Point(0, 1, 2), Quaternion(0, 0, 0, 1))
+      
+        # insert a pose object with a name
+        msg_store.insert_named(POSE_NAME, p)
+ 
+        # get it back with a name
+        stored = msg_store.query_named(POSE_NAME, Pose._type)[0]
+
+        self.assertIsInstance(stored, Pose)
+        self.assertEqual(stored.position.x, p.position.x)
+        self.assertEqual(stored.position.y, p.position.y)
+        self.assertEqual(stored.position.z, p.position.z)
+
+        self.assertEqual(stored.orientation.x, p.orientation.x)
+        self.assertEqual(stored.orientation.y, p.orientation.y)
+        self.assertEqual(stored.orientation.z, p.orientation.z)
+        self.assertEqual(stored.orientation.w, p.orientation.w)
+
+        p.position.x = 666
+
+        msg_store.update_named(POSE_NAME, p)
+
+        # get it back with a name
+        updated = msg_store.query_named(POSE_NAME, Pose._type)[0]
+
+        self.assertEqual(updated.position.x, p.position.x)
+
+        # # try to get it back with an incorrect name
+        wrong_name = "thid name does not exist in the datacentre"
+        none_item = msg_store.query_named(wrong_name, Pose._type)[0]
+        self.assertIsNone(none_item)
+
+        # # get all non-existant typed objects, so get an empty list back
+        none_query = msg_store.query( "not my type")
+        self.assertEqual(len(none_query), 0)
+        
+        # URGENT TODO: must remove the item or unittest only really valid once
+
+        
+
+    
+    
+if __name__ == '__main__':
+    import rostest
+    PKG = 'ros_datacentre'
+    rostest.rosrun(PKG, 'test_message_store_proxy', TestMessageStoreProxy)
+    
+ 
