eae8b75838933e5a06d9e28e4971345b4ae67999
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-04 09:59:30 +0100

    Added examples of id-based operations.

--- ros_datacentre/scripts/example_message_store_client.py
+++ ros_datacentre/scripts/example_message_store_client.py
@@ -18,15 +18,37 @@ if __name__ == '__main__':
     p = Pose(Point(0, 1, 2), Quaternion(3, 4,  5, 6))
   
     try:
-        # insert a pose object with a name
-        msg_store.insert_named("my favourite pose", p)
+
+
+        # insert a pose object with a name, store the id from db
+        p_id = msg_store.insert_named("my favourite pose", p)
  
+        # you don't need a name (note that this p_id is different than one above)
+        p_id = msg_store.insert(p)
+
+
         # get it back with a name
         print msg_store.query_named("my favourite pose", Pose._type)
 
         p.position.x = 666
 
+        # update it with a name
         msg_store.update_named("my favourite pose", p)
+
+        p.position.y = 2020
+
+        # update the other inserted one using the id
+        msg_store.update_id(p_id, p)
+
+        stored_p, meta = msg_store.query_id(p_id, Pose._type)
+
+        assert stored_p.position.x == 666
+        assert stored_p.position.y == 2020
+        print "stored object ok"
+        print "stored object inserted at %s (UTC rostime) by %s" % (meta['inserted_at'], meta['inserted_by'])
+        print "stored object last updated at %s (UTC rostime) by %s" % (meta['last_updated_at'], meta['last_updated_by'])
+
+        # some other things you can do...
 
         # get it back with a name
         # print msg_store.query_named("my favourite pose", Pose._type)
