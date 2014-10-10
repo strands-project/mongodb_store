2227884fb50e28f164c5a01b2c324451da34ee7c
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-10-10 13:32:12 +0100

    Sim time is now awaited correctly.

--- mongodb_store/scripts/mongodb_play.py
+++ mongodb_store/scripts/mongodb_play.py
@@ -4,6 +4,7 @@
 import rospy
 import mongodb_store.util as mg_util
 import sys
+import time
 import pymongo
 from multiprocessing import Process
 import calendar
@@ -33,66 +34,103 @@ def ros_time_strftime(rt, format):
     """ converts a ros time to a datetime and calls strftime on it with the given format """
     return to_datetime(rt).strftime(format)
 
-class TopicPlayer(object):
+
+class PlayerProcess(object):
+    
+    def __init__(self, event, start_time, end_time):
+        super(PlayerProcess, self).__init__()
+
+        self.event = event
+
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+        self.running = multiprocessing.Value('b', True)
+        self.player_process = multiprocessing.Process(target=self.run, args=[self.running])
+        
+    def start(self):        
+        self.player_process.start()
+
+    def stop(self):        
+        self.running.value = False
+
+    def join(self):        
+        self.player_process.join()
+
+    def is_running(self):
+        return self.running.value
+
+class TopicPlayer(PlayerProcess):
     """ """
-    def __init__(self, mongodb_host, mongodb_port, db_name, collection_name, start_time, end_time):
-        super(TopicPlayer, self).__init__()
+    def __init__(self, mongodb_host, mongodb_port, db_name, collection_name, event, start_time, end_time):
+        super(TopicPlayer, self).__init__(event, start_time, end_time)
         
         self.mongodb_host = mongodb_host
         self.mongodb_port = mongodb_port
         self.db_name = db_name
         self.collection_name = collection_name
 
-        self.start_time = start_time
-        self.end_time = end_time
-
-        self.player_process = multiprocessing.Process(target=self.run)
-        # self.player_process = threading.Thread(target=self.run)
-        self.run = True
-
-    def start(self):        
-        self.player_process.start()
-
 
     def init(self):
         """ Called in subprocess to do process-specific initialisation """
+
+        rospy.init_node("mongodb_playback_%s" % self.collection_name) 
 
         # clear signal handlers in this child process, rospy will handle signals for us
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+        self.mongo_client=MongoClient(self.mongodb_host, self.mongodb_port)
+        self.collection = self.mongo_client[self.db_name][self.collection_name]
 
-    def run(self):
+
+    def run(self, running):
 
         self.init()
 
-        while self.run:
-            rospy.loginfo(self.collection_name)
+        rospy.loginfo('Topic playback ready %s %s' % (self.collection.name, rospy.get_param('use_sim_time')))
+
+        while running.value:
+            rospy.loginfo('%s %s' % (self.collection.name, rospy.get_param('use_sim_time')))
             rospy.sleep(1)
 
-    def stop(self):
-        self.run = False
-
-    def join(self):
-        self.player_process.join()
+        self.mongo_client.disconnect()
+        rospy.loginfo('Topic playback finished %s' % self.collection.name)            
 
 
 
-class ClockPlayer(object):
+class ClockPlayer(PlayerProcess):
     """ Plays a clock message in a separate thread"""
-    def __init__(self, start_time, end_time, pre_roll = rospy.Duration(0), post_roll = rospy.Duration(0)):
-        super(ClockPlayer, self).__init__()
+    def __init__(self, event, start_time, end_time, pre_roll = rospy.Duration(0), post_roll = rospy.Duration(0)):
+        super(ClockPlayer, self).__init__(event, start_time, end_time)
         self.start_time = start_time
         self.end_time = end_time
         self.pre_roll = pre_roll
-        self.post_roll = post_roll
-        self.clock_thread = threading.Thread(target=self.run)
-        self.run = True
+        self.post_roll = post_roll        
+      
+    def init(self):
 
-    def start(self):
-        self.clock_thread.start()
+        # we handle shutdown for this process
+        signal.signal(signal.SIGTERM, signal.SIG_DFL)
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    def run(self):
+        # make sure this node doesn't use sim time
+        rospy.set_param('use_sim_time', False)
+
+        rospy.init_node('mongodb_playback_clock_player')
+    
+        # switch to simulated time, note that as this is after the init_node, this node DOES NOT use sim time
+        rospy.set_param('use_sim_time', True)
+
+        # notify everyone else that they can move on
+        self.event.set()
+
+
+    def run(self, running):
+
+        self.init()
+
         start = self.start_time - self.pre_roll
         end = self.end_time + self.post_roll
         
@@ -103,12 +141,12 @@ class ClockPlayer(object):
         clock_msg = Clock(clock=start)
 
         # timing details, should be moved to constructor parameters
-        updates_hz = 1000.0
+        updates_hz = 10000.0
         rate = rospy.Rate(updates_hz)
         # this assumes close to real-time playback
         update = rospy.Duration(1.0 / updates_hz)
 
-        while self.run and clock_msg.clock <= end:
+        while running.value and clock_msg.clock <= end:
             # publish time
             clock_pub.publish(clock_msg)
 
@@ -117,19 +155,11 @@ class ClockPlayer(object):
 
             rate.sleep()
 
-        self.run = False
+        rospy.loginfo('All done here')
+        running.value = False
 
 
-    def join(self):
-        self.clock_thread.join()
-
-    def stop(self):
-        self.run = False
-
-
-    def is_running(self):
-        return self.run
-
+ 
 
 
 class MongoPlayback(object):
@@ -171,65 +201,72 @@ class MongoPlayback(object):
         start_time = to_ros_time(min(map(min_time, collections)))
         end_time =  to_ros_time(max(map(max_time, collections)))
 
-        rospy.loginfo('Playing back from %s' % to_datetime(start_time))
-        rospy.loginfo('.............. to %s' % to_datetime(end_time))
+        # we don't need a connection any more
+        self.mongo_client.disconnect()
 
-        # create playback objects
-        self.players = map(lambda c: TopicPlayer(self.mongodb_host, self.mongodb_port, database_name, c, start_time, end_time), topics)       
+        # rospy.loginfo('Playing back from %s' % to_datetime(start_time))
+        # rospy.loginfo('.............. to %s' % to_datetime(end_time))
 
-        # create clock thread
+        self.event = multiprocessing.Event()
+
+        # create clock thread        
         pre_roll = rospy.Duration(10)
         post_roll = rospy.Duration(10)
-        self.clock_thread = ClockPlayer(start_time, end_time, pre_roll, post_roll)
+        self.clock_player = ClockPlayer(self.event, start_time, end_time, pre_roll, post_roll)
+
+        # create playback objects
+        self.players = map(lambda c: TopicPlayer(self.mongodb_host, self.mongodb_port, database_name, c, self.event, start_time - pre_roll, end_time + post_roll), topics)       
+
 
     def start(self):
-        self.clock_thread.start()
+        self.clock_player.start()
+
+        self.event.wait()
+        self.event.clear()
+
         for player in self.players:
             player.start()
 
 
     def join(self):
-        while not rospy.is_shutdown() and self.clock_thread.is_running():
-            rospy.sleep(0.2)
 
-        self.clock_thread.join()
+        self.clock_player.join()
         for player in self.players:
             player.join()
 
 
 
     def stop(self):
-        rospy.logdebug('Shutdown requested')
-        self.clock_thread.stop()
+        print 'Shutdown requested'
+        self.clock_player.stop()
         for player in self.players:
             player.stop()
 
 
     def is_running(self):
-        return self.clock_thread.is_running()
+        return self.clock_player.is_running()
+
 
 def main(argv):
-    # make sure this node doesn't use sim time
-    rospy.set_param('use_sim_time', False)
-
-    rospy.init_node("mongodb_playback") 
-    
-    # switch to simulated time, note that as this is after the init_node, this node DOES NOT use sim time
-    rospy.set_param('use_sim_time', True)
 
     playback = MongoPlayback()
 
-    rospy.on_shutdown(playback.stop)
-    
+    def signal_handler(signal, frame):
+        playback.stop()
+
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+
     myargv = rospy.myargv(argv=argv)
     topics = myargv[1:] 
 
     database_name = 'roslog'
     playback.setup(database_name, set(topics))
     playback.start()
+    
     playback.join()
-
-    rospy.set_param('use_sim_time', False)
+    
 
 
 # processes load main so move init_node out 
