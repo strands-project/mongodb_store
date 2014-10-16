dd5e98d31120e7bdce931ac14beeb965a83c2c3e
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-10-16 15:27:22 +0100

    Adding meta information into C++ logging.

--- mongodb_store/include/mongodb_store/util.h
+++ mongodb_store/include/mongodb_store/util.h
@@ -37,20 +37,22 @@ namespace mongodb_store {
 template<typename MsgType> 
 void add_meta_for_msg(const typename MsgType::ConstPtr & _msg,  mongo::BSONObjBuilder & _builder) {
 
+  	mongo::BSONObjBuilder meta;
 
+  	ros::Time now = ros::Time::now();
+  	mongo::Date_t nowDate(now.sec * 1000 + now.nsec / 1000000);	
+	meta.append("inserted_at", nowDate);
 
+  	std::string type(ros::message_traits::DataType<MsgType>::value());
+	meta.append("stored_type", type);
 
-	// doc["_meta"]["inserted_at"]
-	// ros::Time::now()
-	// Date_t stamp = msg_in.header.stamp.sec * 1000 + msg_in.header.stamp.nsec / 1000000;
-	// AND FIX OTHER FILES WHEN SOLVED
+	size_t slashIndex = type.find('/');
+	std::string package = type.substr(0, slashIndex);
+	std::string name = type.substr(slashIndex + 1, type.length() - slashIndex - 1);
+	std::string pythonName = package + ".msg._" + name + "." + name;
+	meta.append("stored_class", pythonName);
 
-	// doc["_meta"]["stored_type"]
-	// ros::message_traits::DataType<MsgType>::value();
-
-	// and the other will do 
-	// http://www.cplusplus.com/reference/string/string/find/
-
+	_builder.append("_meta", meta.obj());
 
 }
 
