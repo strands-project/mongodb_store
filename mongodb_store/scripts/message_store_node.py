900df1cbb4d884270cc59189e13c6c6412cf95d0
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-01-24 14:52:33 +0900

    fix error on querying extra servers

--- mongodb_store/scripts/message_store_node.py
+++ mongodb_store/scripts/message_store_node.py
@@ -239,13 +239,13 @@ class MessageStore(object):
  	    # this is a list of entries in dict format including meta
         #projection_query_dict = dc_util.string_pair_list_to_dictionary(req.projection_query)
 
-        entries =  dc_util.query_message(collection, obj_query, sort_query_tuples, {},req.single, req.limit)
+        entries =  dc_util.query_message(collection, obj_query, sort_query_tuples, {}, req.single, req.limit)
 
         # keep trying clients until we find an answer
         for extra_client in self.extra_clients:
             if len(entries) == 0:
                 extra_collection = extra_client[req.database][req.collection]
-                entries =  dc_util.query_message(extra_collection, obj_query, sort_query_tuples, req.single, req.limit)
+                entries =  dc_util.query_message(extra_collection, obj_query, sort_query_tuples, {}, req.single, req.limit)
                 if len(entries) > 0:
                     rospy.loginfo("found result in extra datacentre")
             else:
