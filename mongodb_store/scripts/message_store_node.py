4ba3926b21154f4ff7871aa47846d07c5f8bdfbd
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-02-10 09:03:28 +0900

    mongodb_store: use launch for test

--- mongodb_store/scripts/message_store_node.py
+++ mongodb_store/scripts/message_store_node.py
@@ -30,23 +30,24 @@ class MessageStore(object):
         if str(local_timeout).lower() == "none":
             local_timeout = None
 
-	if use_daemon:
-            db_host = rospy.get_param('mongodb_host')
-            db_port = rospy.get_param('mongodb_port')
+        # wait for hostname and port for mongodb server
+        for _ in range(10):
+            if rospy.has_param('mongodb_host') and rospy.has_param('mongodb_port'):
+                break
+            rospy.sleep(1.0)
+        db_host = rospy.get_param('mongodb_host')
+        db_port = rospy.get_param('mongodb_port')
+
+        if use_daemon:
             is_daemon_alive = dc_util.check_connection_to_mongod(db_host, db_port)
             if not is_daemon_alive:
                 raise Exception("No Daemon?")
-        else:
-	  if use_localdatacenter:
+        elif use_localdatacenter:
             rospy.loginfo('Waiting for local datacentre (timeout: %s)' % str(local_timeout))
             have_dc = dc_util.wait_for_mongo(local_timeout)
             if not have_dc:
                 raise Exception("No Datacentre?")
-          # move these to after the wait_for_mongo check as they may not be set before the db is available
-          db_host = rospy.get_param('mongodb_host')
-          db_port = rospy.get_param('mongodb_port')
 
-        
         self.keep_trash = rospy.get_param('mongodb_keep_trash', True)
 
         self._mongo_client=MongoClient(db_host, db_port)
